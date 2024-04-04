82e6f4958b9c5169832be597e8ebc48b38040c19
Limkwan1004 <janyouns@gmail.com>
2024-04-04 17:51:03 +0900

    [Feat] Subject Circle 4

--- Assets/3.Script/Subject/SubjectState.cs
+++ Assets/3.Script/Subject/SubjectState.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public abstract class SubjectState : MonoBehaviour
 {
+    protected CircleCollider2D _leftCol;
+    protected CircleCollider2D _rightCol;
+    protected CircleCollider2D _upCol;
+    protected CircleCollider2D _downCol;
     public abstract void OnEnter();
     public abstract void OnUpdate();
     public abstract void OnFixedUpdate();
